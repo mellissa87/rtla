0e3a727ed298dffa19c39a9cd2306a0400009bcc
Piotr Bochenski <bochenski.piotr@gmail.com>
2015-11-22 16:18:34 +0100

    change structure of ErrorLogDaoIT

--- rtla-cassandra/src/test/java/com/github/b0ch3nski/rtla/cassandra/dao/ErrorLogDaoIT.java
+++ rtla-cassandra/src/test/java/com/github/b0ch3nski/rtla/cassandra/dao/ErrorLogDaoIT.java
@@ -5,6 +5,7 @@ import com.github.b0ch3nski.rtla.cassandra.CassandraTestingHelper;
 import com.github.b0ch3nski.rtla.common.model.SimplifiedLog;
 import com.github.b0ch3nski.rtla.common.utils.RandomLogFactory;
 import org.junit.*;
+import org.junit.rules.ExternalResource;
 
 import java.util.List;
 
@@ -15,30 +16,38 @@ import static org.hamcrest.core.Is.is;
  * @author bochen
  */
 public class ErrorLogDaoIT {
+    private static final long TTL = 3600L;
+    private static ErrorLogDao errorLogDao;
 
-    private ErrorLogDao errorLogDao;
+    @ClassRule
+    public static final ExternalResource RESOURCE = new ExternalResource() {
+        @Override
+        protected void before() throws Throwable {
+            CassandraTestingHelper.launchCassandra();
+            errorLogDao = new ErrorLogDao(CassandraTestingHelper.getConfig(), TTL);
+        }
 
-    @BeforeClass
-    public static void setUpOnce() {
-        CassandraTestingHelper.launchCassandra();
-    }
+        @Override
+        protected void after() {
+            errorLogDao.shutdown();
+        }
+    };
 
     @Before
     public void setUp() {
-        errorLogDao = new ErrorLogDao(CassandraTestingHelper.getConfig(), 3600L);
         errorLogDao.truncateTable();
     }
 
     @Test
     public void shouldSaveAndRetrieveErrorLog() {
-        SimplifiedLog before = RandomLogFactory.create(Level.ERROR);
+        SimplifiedLog toSave = RandomLogFactory.create(Level.ERROR);
 
-        errorLogDao.save(before);
+        errorLogDao.save(toSave);
 
-        List<SimplifiedLog> after = errorLogDao.get(before.getHostName());
+        List<SimplifiedLog> retrieved = errorLogDao.get(toSave.getHostName());
 
         assertThat(errorLogDao.countAllElements(), is(1L));
-        assertThat(after.size(), is(1));
-        assertThat(after.get(0), is(before));
+        assertThat(retrieved.size(), is(1));
+        assertThat(retrieved.get(0), is(toSave));
     }
 }
