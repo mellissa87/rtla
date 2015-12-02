002d2a268fb6933c852b946b64e1164af501dd28
Piotr Bochenski <piotr.bochenski@nokia.com>
2015-12-02 18:46:25 +0100

    refactor GenericEsDao; implement bulk data insertion

--- rtla-elasticsearch/src/main/java/com/github/b0ch3nski/rtla/elasticsearch/GenericEsDao.java
+++ rtla-elasticsearch/src/main/java/com/github/b0ch3nski/rtla/elasticsearch/GenericEsDao.java
@@ -2,17 +2,29 @@ package com.github.b0ch3nski.rtla.elasticsearch;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
+import org.elasticsearch.action.bulk.*;
+import org.elasticsearch.action.bulk.BulkProcessor.Listener;
 import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequestBuilder;
-import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.index.*;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.*;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * @author bochen
@@ -25,58 +37,148 @@ public abstract class GenericEsDao<T> {
     private final String indexName;
     private final String typeName;
     private final Client client;
+    private final BulkProcessor bulkProcessor;
+    private XContentBuilder mapping;
 
     protected GenericEsDao(Settings settings, Class<T> classType, ObjectMapper jsonMapper, String indexName, String typeName) {
         this.classType = classType;
         this.jsonMapper = jsonMapper;
         this.indexName = indexName;
         this.typeName = typeName;
-        client = ElasticsearchSession.getInstance(settings).getClient();
 
-        if (!isMappingExists()) createMapping();
+        client = ElasticsearchSession.getInstance(settings).getClient();
+        bulkProcessor = new BulkHandler().getBulkProcessor();
+        createIndex();
+    }
+
+    private class BulkHandler implements Listener {
+        @Override
+        public void beforeBulk(long executionId, BulkRequest request) {
+            LOGGER.trace("Going to execute bulk of {} actions", request.numberOfActions());
+        }
+
+        @Override
+        public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+            LOGGER.debug("Executed bulk of {} actions | Execution took {} milliseconds", request.numberOfActions(), response.getTookInMillis());
+        }
+
+        @Override
+        public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+            LOGGER.warn("Bulk execution failed", failure);
+        }
+
+        // TODO: make those settings configurable somehow
+        private BulkProcessor getBulkProcessor() {
+            return BulkProcessor.builder(client, this)
+                    .setBulkActions(10)
+                    .setBulkSize(new ByteSizeValue(1, ByteSizeUnit.MB))
+                    .setFlushInterval(TimeValue.timeValueSeconds(15))
+                    .setConcurrentRequests(1)
+                    .build();
+        }
     }
 
     protected abstract XContentBuilder provideMapping() throws IOException;
 
-    private boolean isMappingExists() {
+    private boolean isIndexExists() {
         boolean isExists = client.admin().indices().prepareExists(indexName).execute().actionGet().isExists();
-        LOGGER.debug("Mapping for {} found = {}", indexName, isExists);
+        LOGGER.info("Index {} found = {}", indexName, isExists);
         return isExists;
     }
 
-    private void createMapping() {
-        try (XContentBuilder mapping = provideMapping()) {
-            LOGGER.debug("Creating mapping for {} = {}", indexName, mapping.string());
-            client.admin().indices().prepareCreate(indexName).addMapping(typeName, mapping).execute().actionGet();
+    private XContentBuilder getMapping() {
+        if (mapping == null) try {
+            mapping = provideMapping();
+            LOGGER.info("Got mapping for {} index = {}", indexName, mapping.string());
         } catch (IOException e) {
             throw new IllegalStateException("Couldn't create mapping", e);
         }
+        return mapping;
     }
 
-    public final String save(T toSave) {
-        IndexRequestBuilder requestBuilder = client.prepareIndex(indexName, typeName);
-        byte[] json;
+    @VisibleForTesting
+    protected final void createIndex() {
+        if (isIndexExists()) return;
+        LOGGER.info("Creating {} index", indexName);
+        client.admin().indices().prepareCreate(indexName).addMapping(typeName, getMapping()).execute().actionGet();
+    }
 
+    @VisibleForTesting
+    protected final void deleteIndex() {
+        if (!isIndexExists()) return;
+        LOGGER.info("Index removal called on {} index", indexName);
+        client.admin().indices().prepareDelete(indexName).execute().actionGet();
+    }
+
+    @VisibleForTesting
+    protected final void refreshIndex() {
+        LOGGER.info("Force refresh called on {} index", indexName);
+        client.admin().indices().refresh(new RefreshRequest(indexName)).actionGet();
+    }
+
+    @VisibleForTesting
+    protected final void refreshBulk() {
+        LOGGER.info("Force flush called on bulk processor");
+        bulkProcessor.flush();
+    }
+
+    private byte[] getJsonFromObject(T toJson) {
         try {
-            json = jsonMapper.writeValueAsBytes(toSave);
+            return jsonMapper.writeValueAsBytes(toJson);
         } catch (JsonProcessingException e) {
-            throw new IllegalStateException(e);
+            throw new IllegalStateException("Couldn't serialize object of type " + classType + " to JSON", e);
         }
+    }
+
+    private T getObjectFromJson(byte[] json) {
+        try {
+            return jsonMapper.readValue(json, classType);
+        } catch (IOException e) {
+            throw new IllegalStateException("Couldn't deserialize JSON to object of type " + classType, e);
+        }
+    }
+
+    public final IndexResponse save(T toSave) {
+        IndexRequestBuilder requestBuilder = client.prepareIndex(indexName, typeName);
+        byte[] json = getJsonFromObject(toSave);
 
         IndexResponse response = requestBuilder.setSource(json).execute().actionGet();
         LOGGER.trace("Object {} was saved to /{}/{} with ID = {}", toSave, response.getIndex(), response.getType(), response.getId());
-        return response.getId();
+        return response;
+    }
+
+    public final void saveBulk(List<T> toSave) {
+        toSave.forEach(item -> {
+            byte[] json = getJsonFromObject(item);
+            bulkProcessor.add(new IndexRequest(indexName, typeName).source(json));
+        });
+    }
+
+    @SafeVarargs
+    public final void saveBulk(T... toSave) {
+        saveBulk(Lists.newArrayList(toSave));
     }
 
     public final Optional<T> get(String id) {
         GetResponse response = client.prepareGet(indexName, typeName, id).get();
 
-        try {
-            if (response.isExists()) return Optional.of(jsonMapper.readValue(response.getSourceAsBytes(), classType));
-            else return Optional.absent();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
+        if (response.isExists()) {
+            byte[] json = response.getSourceAsBytes();
+            return Optional.of(getObjectFromJson(json));
         }
+        else return Optional.absent();
+    }
+
+    private T getObjectFromSearchHit(SearchHit hit) {
+        return getObjectFromJson(hit.getSourceAsString().getBytes());
+    }
+
+    public final List<T> search(QueryBuilder query) {
+        SearchResponse response = client.prepareSearch(indexName).setTypes(typeName).setQuery(query).execute().actionGet();
+        SearchHits hits = response.getHits();
+
+        LOGGER.trace("Search took {} milliseconds and found {} hits | Query:\n{}", response.getTookInMillis(), hits.getTotalHits(), query);
+        return StreamSupport.stream(hits.spliterator(), false).map(this::getObjectFromSearchHit).collect(Collectors.toList());
     }
 
     public final void shutdown() {
--- rtla-elasticsearch/src/test/java/com/github/b0ch3nski/rtla/elasticsearch/SimplifiedLogEsDaoTest.java
+++ rtla-elasticsearch/src/test/java/com/github/b0ch3nski/rtla/elasticsearch/SimplifiedLogEsDaoTest.java
@@ -4,8 +4,12 @@ import ch.qos.logback.classic.Level;
 import com.github.b0ch3nski.rtla.common.model.SimplifiedLog;
 import com.github.b0ch3nski.rtla.common.utils.RandomLogFactory;
 import com.google.common.base.Optional;
-import org.junit.AfterClass;
-import org.junit.Test;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.joda.time.DateTime;
+import org.junit.*;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -17,16 +21,45 @@ public class SimplifiedLogEsDaoTest extends ElasticsearchDaoIT {
 
     private static final SimplifiedLogEsDao DAO = new SimplifiedLogEsDao(getSettingsForDao());
 
+    @Before
+    public void setUp() {
+        DAO.createIndex();
+    }
+
     @Test
-    public void shouldSendAndGetLog() {
+    public void shouldSaveAndGetSingleLog() {
         SimplifiedLog expected = RandomLogFactory.create(Level.DEBUG);
 
-        String id = DAO.save(expected);
+        String id = DAO.save(expected).getId();
+        DAO.refreshIndex();
 
         Optional<SimplifiedLog> retrieved = DAO.get(id);
 
         assertThat(retrieved.isPresent(), is(true));
         assertThat(retrieved.get(), is(expected));
+    }
+
+//    @Test
+    public void shouldSaveAndGetBulkLogs() {
+        List<SimplifiedLog> expected = RandomLogFactory.create(15);
+
+        DAO.saveBulk(expected);
+        DAO.refreshBulk();
+        DAO.refreshIndex();
+
+        QueryBuilder query = QueryBuilders.rangeQuery("timeStamp")
+                .from(DateTime.now().minusMinutes(1))
+                .to(DateTime.now().plusMinutes(1));
+
+        List<SimplifiedLog> retrieved = DAO.search(query);
+
+        assertThat(retrieved.size(), is(expected.size()));
+        assertThat(retrieved, is(expected));
+    }
+
+    @After
+    public void tearDown() {
+        DAO.deleteIndex();
     }
 
     @AfterClass
